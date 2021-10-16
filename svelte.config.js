f3caab840b2f705616d6903e0438084805841604
Rajmohan S <rajs5@publicisgroupe.net>
2021-10-16 22:18:24 +0530

    first commit - vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-vercel';
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
-		adapter: adapter(),
+		adapter: vercel(),
 		target: '#svelte'
 	}
 };
